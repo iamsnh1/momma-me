c6c211f0a43eadb10a34eba82b983f6c023dc0ce
snhpower12 <snhpower12@gmail.com>
2026-01-15 22:50:05 +0530

    Fix duplicate state declaration

--- components/AdminProductsManagement.tsx
+++ components/AdminProductsManagement.tsx
@@ -57,7 +57,6 @@ export default function AdminProductsManagement() {
   })
   
   const [isDragging, setIsDragging] = useState(false)
-  
   const [isUploading, setIsUploading] = useState(false)
   
   const handleFileUpload = async (files: FileList | null) => {
